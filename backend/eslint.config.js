a31cc92e47c07b91e9187fa8d4f5fa5e99ee9a3c
Runior Elio Machicado Choque <ru_mc1992@hotmail.com>
2026-07-09 11:12:24 -0400

    ci: agregar primer pipeline de integración continua

---
+++ backend/eslint.config.js
@@ -0,0 +1,35 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+const tseslint = require("typescript-eslint");
+
+module.exports = tseslint.config(
+  {
+    ignores: [
+  "dist/**",
+  "node_modules/**",
+  "eslint.config.js",
+  "prisma.config.ts"
+]
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["src/**/*.ts"],
+
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
+
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+      "no-undef": "off"
+    }
+  }
+);
